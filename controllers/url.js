9bcae1b2594eb1697422f7c3ef05582b7e96a4bc
Adnan7209 <mdadnan28713@gmail.com>
2024-02-10 21:17:15 +0530

    ejs installed

--- controllers/url.js
+++ controllers/url.js
@@ -2,17 +2,27 @@ const shortid = require("shortid");
 // import { nanoid } from 'nanoid'
 const URL = require("../models/url");
 
-const handleGenerateNewShortUrl = async(req,res) => {
-    const body = req.body;
-    if(!body.url)
-        return res.status(400).json({error:"url is required"});
-    const shortID = shortid();
-    await URL.create({
-        shortId:shortID,
-        redirectUrl:body.url,
-        visitHistory:[],
-    })
-    return res.json({id:shortID});
-}
+const handleGenerateNewShortUrl = async (req, res) => {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "url is required" });
+  const shortID = shortid();
+  await URL.create({
+    shortId: shortID,
+    redirectUrl: body.url,
+    visitHistory: [],
+  });
+  return res.json({ id: shortID });
+};
 
-module.exports = {handleGenerateNewShortUrl}
+const handleGetAnalytics = async (req, res) => {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne( {shortId} );
+//   if(!result)
+//     return res.status(400).json({error:"cannot find id"});
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    Analytics: result.visitHistory,
+  });
+};
+
+module.exports = { handleGenerateNewShortUrl, handleGetAnalytics };
--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require("express");
-const {connectToMongoDb} = require("./connect");
+const { connectToMongoDb } = require("./connect");
 
 const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
 connectToMongoDb("mongodb://127.0.0.1:27017/short-url")
-.then(() => console.log("mongodb connected"))
-.catch((err) => console.log("mongo error ", err));
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log("mongo error ", err));
 
 app.use(express.json());
-app.use("/url",urlRoute);
+app.use("/url", urlRoute);
 
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  return res.redirect(entry.redirectUrl);
+});
 
-
-
-
-app.listen(PORT,()=>console.log(`server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
